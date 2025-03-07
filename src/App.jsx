531b0c63a5741e6acd99612fe6ce1ca8e857f34e
aady07 <103576677+aady07@users.noreply.github.com>
2025-03-07 10:15:48 +0530

    Added Vercel and modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import Intro from "./components/Intro";
 
 const App = () => {
   return (
     <div className="App">
       <Intro />
+      <Analytics />
      
     </div>
   );
